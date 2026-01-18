89a165e3f05bd347154b26d49402997dc0c7b56b
youssefkhaled_74 <iyoussefbusinesstowork@gmail.com>
2026-01-18 03:13:44 -0800

    fix: Add missing cast for phone_verified_at attribute in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,6 +52,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'phone_verified_at' => 'datetime',
         'password' => 'hashed',
         'birth_date' => 'date',
     ];
