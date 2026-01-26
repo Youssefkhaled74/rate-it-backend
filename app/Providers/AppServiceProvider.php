990e5bde09bd5982ad7888646dedb4bf4ce8a516
youssefkhaled_74 <iyoussefbusinesstowork@gmail.com>
2026-01-26 01:34:36 -0800

    feat: Register AdminModuleServiceProvider in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Register Admin Module
+        $this->app->register(\App\Providers\AdminModuleServiceProvider::class);
+
         // Bind module service interfaces to implementations
         $this->app->bind(OnboardingServiceInterface::class, OnboardingService::class);
 
